b86c556e4ad28b9b45d0ba5b178dd7727d9538eb
Tan Jun Hui <akimotojh@gmail.com>
2023-01-13 11:56:41 +0800

    Added webpack config

--- bundle-build-tool/src/index.js
+++ bundle-build-tool/src/index.js
@@ -1,4 +1,11 @@
 // test
 import { camelCase } from "lodash";
+import vars from "./style.scss";
 
 console.log(camelCase("hello world"));
+console.log(vars.primaryColor);
+
+window.addEventListener("load", function() {
+    this.alert("it's loaded");
+    this.document.getElementsByTagName("body")[0].style.color = vars.primaryColor;
+})
